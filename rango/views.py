e890f6de149917f6750cde502d8f4c3965c7f9d9
Altamash Khan <3021255k@student.gla.ac.uk>
2026-02-13 12:11:04 +0000

    Chapter 3: rendered template for index page.

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return render(request, 'rango/index.html')
+
